9d9df8f2e2fb2ba3b228a2407b63e1a6cbf67369
Heather Ridgill <Heather.Ridgill@gmail.com>
2020-05-31 19:29:01 -0700

    Finished Ring_Buffer - not sure while its failing :(

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,44 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.oldest = 0
+
 
     def append(self, item):
-        pass
+        #Option 1
+        #First check how many elements are there
+        if (len(self.storage) == self.capacity):
+            #Pop it == remove it == GET IT OUTTA THERE!
+            self.storage.pop(self.oldest)
+            #Remember the first index is zero.
+            self.storage.insert(self.oldest, item)
+    
+            # overwrite the oldest item
+            # -1 would be the last item in the array
+            if self.oldest == len(self.storage) -1:
+                self.oldest = 0
+        else:
+                self.storage.append(item)
+
+       
+
+        #Option 2
+        # if len(self.storage) < self.capacity:
+        #    if len(self.storage) == 0:
+        #         self.oldest = 0
+        #     else:
+        #         self.oldest += 1
+        #     self.storage.append(item)
+        
+        # else:
+        #    if self.oldest == self.capacity - 1:
+        #         self.oldest = 0
+        #      else:
+        #         self.oldest += 1
+        #     self.storage[self.oldest] = item
+
+            
 
     def get(self):
-        pass
+        return [item for item in self.storage if item is not None]
