1b7dc963ffaed1a108e69feb3a83045c4906f55c
Heather Ridgill <Heather.Ridgill@gmail.com>
2020-05-31 20:16:52 -0700

    Finished reverse - passed tests

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,5 +38,37 @@ class LinkedList:
 
         return False
 
+
+    def print_helper(self, node, name):
+        if node is None:
+            print(name + ": None")
+        else:
+            print(name + ":" + node.data)
+
+# A -> B -> C -> D -> 0 (INITIAL)
+# D -> C -> B -> A -> 0 (REVERSAL)
+# A <- B <- C <- D <- 0 (MOVES OPPOSITE)
+
+
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            return 
+        # if next node is none 
+        if node.next_node is None:
+        # new head becomes current node
+            self.head = node
+            #next node of the new head = value of prev
+            node.next_node = prev
+        # not at the end
+        else:
+            # saving next in temporary variable
+            next_node = node.next_node
+            # reversing.. point current node to previous node
+            # prev node becomes current
+            node.next_node = prev
+            self.reverse_list(next_node, node)
+
+
+
+
+
